9deb1d508ff60f9a0cc2ea4241221cef0a5f3a69
damianmartinez92 <damian.martinez@live.com.ar>
2019-11-20 09:45:26 -0300

    cambio prueba

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ class MeliReact extends React.Component {
   render() {
     return (
       <Container isFluid style={{ marginTop: 10 }}>
+        asdkjasdasmdas
         <SearchBox setInputValue={(term) => this.setQuery(term)} onSearchButtonClick={() => this.getProducts()}/>
         <ProductCluster products={this.state.products}/>
       </Container>
